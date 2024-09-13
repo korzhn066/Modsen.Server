52fed732e5a2f7d9f39e9ac03bdd3117d721dc06
korzhn066@gmail.com <korzhn066@gmail.com>
2024-09-13 11:34:48 +0300

    Fix password error answer

--- Modsen.Server.Authentication/Modsen.Server.Authentication.Application/Validators/Authentication/RegisterApplicationUserValidator.cs
+++ Modsen.Server.Authentication/Modsen.Server.Authentication.Application/Validators/Authentication/RegisterApplicationUserValidator.cs
@@ -22,23 +22,11 @@ namespace Modsen.Server.Authentication.Application.Validators.Authentication
 
             RuleFor(registerApplicationUser => registerApplicationUser.RegisterModel.Password)
                 .NotEmpty()
-                .WithMessage("Password is required");
-
-            RuleFor(registerApplicationUser => registerApplicationUser.RegisterModel.Password)
                 .Matches(@"\d")
-                .WithMessage("Password must contain digit");
-
-            RuleFor(registerApplicationUser => registerApplicationUser.RegisterModel.Password)
                 .Matches(@"[A-Z]")
-                .WithMessage("Password must contain upper case letter");
-
-            RuleFor(registerApplicationUser => registerApplicationUser.RegisterModel.Password)
                 .Matches(@"[a-z]")
-                .WithMessage("Password must contain lower case letter");
-
-            RuleFor(registerApplicationUser => registerApplicationUser.RegisterModel.Password)
                 .MinimumLength(6)
-                .WithMessage("Minimum password length is 6");
+                .WithMessage("Password is incorrect");
         }
     }
 }
